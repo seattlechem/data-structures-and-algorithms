41e9e48babde62ec2e90600449ede2d55474e370
seattlechem <seattlechem@yahoo.com>
2018-03-25 11:45:48 -0700

    README.md changed and ENV created

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -5,10 +5,14 @@ class LinkedList:
     """class called LinkedList """
     def __init__(self):
         self.head = None
-   
+
     def insert(self, newNode):
         if self.head is None:
-            self.head - newNode
+            self.head = newNode
 
+
+# instantiate first node with data "john"
+first_node = Node("john")
+# instantiate linkedlist
 linkedlist = LinkedList()
-# when linked list is initialized, head is none
+# 
