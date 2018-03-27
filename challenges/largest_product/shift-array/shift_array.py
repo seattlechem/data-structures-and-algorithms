7958c11f79bcc05a6c19c0a227f7b93fc49edea5
Peter Kim <seattlechem@yahoo.com>
2018-03-27 15:24:05 -0700

    shift_array is refactored

--- challenges/largest_product/shift-array/shift_array.py
+++ challenges/largest_product/shift-array/shift_array.py
@@ -1,15 +1,18 @@
 def insertShiftArray(arr, num):
     if type(arr) is not list or type(num) is not int:
         raise TypeError('Argument(s) invalid.')
-    mid = len(arr) // 2
     lis = list(range(len(arr) + 1))
     for i in range(len(arr)):
         lis[i] = arr[i]
+    length = len(arr)
+    if length % 2:
+        length += 1
+    mid = length // 2
     counter = 1
     while counter <= mid:
-        temp = lis[len(lis) - counter]
-        lis[len(lis) - counter] = lis[len(lis) - (counter + 1)]
-        lis[len(lis) - (counter + 1)] = temp
+        temp = lis[-counter]
+        lis[-counter] = lis[-(counter + 1)]
+        lis[-(counter + 1)] = temp
         counter += 1
     lis[mid] = num
     return lis
--- challenges/largest_product/shift-array/test_shift_array.py
+++ challenges/largest_product/shift-array/test_shift_array.py
@@ -2,8 +2,12 @@ from shift_array import insertShiftArray
 import pytest
 
 
-def test_insert_shift_array():
+def test_insert_shift_array_pass():
     assert insertShiftArray([2, 4, 6, 8], 5) == [2, 4, 5, 6, 8]
+
+
+def test_insert_shift_array_single_element_list():
+    assert insertShiftArray([2, 3, 4], 5) == [2, 3, 5, 4]
 
 
 def test_type_error():
