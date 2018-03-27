fa795519f39c0e6b775f6e9f65a2a5d85ed9225c
Peter Kim <seattlechem@yahoo.com>
2018-03-26 18:23:31 -0700

    if condition to check i & j are not of range

--- challenges/largest_product/largest_product.py
+++ challenges/largest_product/largest_product.py
@@ -2,8 +2,10 @@ def largest_product(arr):
     largest_sum = 0
     for i in range(len(arr)):
         for j in range(len(arr[0])):
-            product = arr[i][j] * arr[i][j + 1]
-            product2 = arr[i][j] * arr[i + 1][j]
+            if j != len(arr[0]) - 1:
+                product = arr[i][j] * arr[i][j + 1]
+            if i != len(arr) - 1:
+                product2 = arr[i][j] * arr[i + 1][j]
             if product > product2:
                 largest_sum = product
             elif product2 > product:
