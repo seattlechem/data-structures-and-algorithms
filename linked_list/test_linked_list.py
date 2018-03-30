8f675188f09d3b5f86335f4241cf1e2149cef4d3
Peter Kim <seattlechem@yahoo.com>
2018-03-30 15:01:31 -0700

    __init__ and conftest.py created

--- linked_list/test_linked_list.py
+++ linked_list/test_linked_list.py
@@ -1,21 +1,4 @@
 import pytest
-from linked_list import LinkedList as ll
-from node import Node
-
-
-@pytest.fixture
-def empty_ll():
-    return ll()
-
-
-@pytest.fixture
-def small_ll():
-    return ll([1, 2, 3, 4])
-
-
-@pytest.fixture
-def test_node():
-    return Node(3, 4)
 
 
 def test_insert_first_node(empty_ll):
