18e80063cee648e070389e43fa55819a053b4fef
seattlechem <seattlechem@yahoo.com>
2018-04-02 07:24:50 -0700

    queue.py complete

---
+++ queue/queue.py
@@ -0,0 +1,25 @@
+from .node import node
+
+
+ class Queue:
+    def __init__(self, iter=[]):
+        self.head = None
+        self._size = 0
+
+        if not isinstance(iter, (list, dict, tuple)):
+            ''' check for iterable'''
+            raise TypeError('It is not iterable.')  
+        for i in iter:
+            self.enqueue(i)
+
+    def enqueue(self, val):
+        ''' add a value, increase size by 1'''
+        self.head = Node(val, self.head)
+        self._size += 1
+
+    def dequeue(self):
+        ''' remove node from the front of queue'''
+        node = self.head
+        self.head = self.head._next
+        self._size -= 1
+        return node
