f7f9ae615b254099dd81764d25cbfb0491600d0f
Peter Kim <seattlechem@yahoo.com>
2018-04-03 18:54:42 -0700

    init and enqueue complete

--- challenges/fifo_animal_shelter/fifo_animal_shelter.py
+++ challenges/fifo_animal_shelter/fifo_animal_shelter.py
@@ -1,15 +1,28 @@
-from .node import Node
+from .queue import Queue
 
 
 class AnimalShelter:
     """ AnimalShelter class"""
     def __init__(self):
+        self.pseudo_queue = Queue()
         self._size = 0
 
     def enqueue(self, val):
         """ add either a dog or cat object """
-        self.push(val)
+        self.pseudo_queue.enqueue(val)
 
-    def dequeue(self, val):
+    def dequeue(self, pref):
         """ return either the longest-waiting cat or dog"""
-        pass
+        # change pref to only first character to upper
+        # type(curret) = Cat
+        if type(self.pseudo_queue.dequeue()) == pref:
+            return self.pseudo_queue.dequeue()
+        
+        current = self.pseudo_queue.dequeue()
+        while type(current) != pref:
+            self.pseudo_queue.enqueue(current)
+            current = self.pseudo_queue.dequeue()
+        
+        # another while loop to back things back to order
+        
+        return current
