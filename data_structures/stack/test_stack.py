5ba98b898e557aea32661dd864cca0aeca941084
seattlechem <seattlechem@yahoo.com>
2018-04-05 23:47:05 -0700

    master clean up

--- data_structures/stack/test_stack.py
+++ data_structures/stack/test_stack.py
@@ -5,7 +5,8 @@ import pytest
 def test_push(empty_stack):
     """ test for push val to empty stack """
     assert empty_stack.top is None
-    assert empty_stack.push(23).val == 23
+    empty_stack.push(23)
+    assert empty_stack.top.val == 23
     assert empty_stack._size == 1
 
 
