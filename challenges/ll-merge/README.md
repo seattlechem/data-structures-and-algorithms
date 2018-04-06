9ac99e6bcd1202d73fac999fc31167ea2c89f67f
seattlechem <seattlechem@yahoo.com>
2018-04-05 22:03:10 -0700

    test completed

--- challenges/ll-merge/README.md
+++
@@ -1,4 +0,0 @@
-### Merge two linked lists (Challenge 8)
-## Write a function called mergeLists which takes two linked lists as arguments. Zip the two linked lists together into one so that the nodes alternate between the two lists and return a reference to the head of the single list. Try and keep additional space down to O(1). You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
-
-![image](../../assets/8_ll_merge.jpg)
--- challenges/ll_merge/test_ll_merge.py
+++ challenges/ll_merge/test_ll_merge.py
@@ -1,12 +1,33 @@
-# import pytest
-from .linked_list import LinkedList
 from .ll_merge import merge_lists
-# from .node import Node
 
 
-def test_simple_merge_true():
-    ll1 = LinkedList([1, 3, 5])
-    ll2 = LinkedList([2, 4, 6])
+def test_simple_merge_true(ll1, ll2):
+    '''
+    test for merging two linked list and
+    assert its return head value
+    '''
 
     head = merge_lists(ll1, ll2)
     assert head.val == 1
+
+
+def test_merge_ll1_only_head(ll1_one, ll2):
+    '''
+    test to confirm if the merge_lists function
+    breaks, and not runs into exception, and return the head of linked-list1
+    when ll1 has only one node.
+    '''
+
+    head = merge_lists(ll1_one, ll2)
+    assert ll1_one._len == 1
+    assert head.val == 1
+
+
+def test_merge_head_next_value_check(ll1, ll2):
+    '''
+    test to confirm if the value of node next to the result head
+    is the value of head of ll2. this confirms if the merge is occuring
+    as alternative order.
+    '''
+    head = merge_lists(ll1, ll2)
+    assert head._next.val == ll2.head.val
