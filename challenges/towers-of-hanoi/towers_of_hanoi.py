114922bd7d5f01bce00b58e449dda8993b501a62
Peter Kim <seattlechem@yahoo.com>
2018-04-11 19:10:31 -0700

    initial attempt

--- challenges/towers-of-hanoi/towers_of_hanoi.py
+++ challenges/towers-of-hanoi/towers_of_hanoi.py
@@ -1,2 +1,18 @@
-def towers_of_hanoi(n):
-    pass
+def towers_of_hanoi(num, A='A', B='B', C='C'):
+    counter = 1
+    while num > 0:
+        # import pdb; pdb.set_trace()
+        towers_of_hanoi(num, A, C, B)
+        _move_disk(counter, A, C)
+        counter += 1
+        num -= 1
+        towers_of_hanoi(num-1, B, C, A)
+    return
+
+
+def _move_disk(counter, source, destination):
+    print('Disk {} moved from {} to {}'.format(counter, source, destination))
+
+
+# if __name__ == "__main__":
+#     main()
