15fdb68eb59765bfae20f86820490578da8b1856
Peter Kim <seattlechem@yahoo.com>
2018-04-26 12:48:16 -0700

    pre_order / post_order test cases for k_tree complete

--- data_structures/k_tree/test_k_tree.py
+++ data_structures/k_tree/test_k_tree.py
@@ -13,3 +13,17 @@ def test_insert_small_ktree_no_parent(small_tree):
     """This tests if ValueError is raised when passing no parent."""
     with pytest.raises(ValueError):
         small_tree.insert(8)
+
+
+def test_pre_order_traverse(small_tree):
+    """Confirm pre_order traversal."""
+    ls = []
+    small_tree.pre_order(lambda n: ls.append(n.val))
+    assert ls == [5, 9, 3]
+
+
+def test_post_order_traversal(small_tree):
+    """Confirm post_order traversal."""
+    ls = []
+    small_tree.post_order(lambda n: ls.append(n.val))
+    assert ls == [9, 3, 5]
