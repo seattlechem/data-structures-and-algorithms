05b9844a633d5c74130019c2d10721fccd88fafe
seattlechem <seattlechem@yahoo.com>
2018-04-30 06:26:52 -0700

    code clean up

--- data_structures/hash_table/hash_table.py
+++ data_structures/hash_table/hash_table.py
@@ -1,7 +1,6 @@
 """Hash Table Class"""
-
-
 from functools import reduce
+
 
 class HashTable:
     def __init__(self, max_size=1024):
@@ -11,30 +10,15 @@ class HashTable:
     def hash_key(self, key):
         if type(key) is not str:
             raise TypeError
-
-        # iterate through key, and convert each char to ascii char code
-        # sum all char codes for a total int value
-        # return => mod total by number of buckets
-
-        # return reduce(lambda a, b: a + ord(b), list(key), 0) % self.buckets
-
+        # ord return an iteger representing the unicode code point of that
+        # given character
         sum = 0
         for char in key:
             sum += ord(char)
         return sum % self.buckets
 
     def set(self, key, val):
-        # hash the key; get a location for the bucket to insert into
-        # set val into bucket
-
-        # You will handle collissions here...
-        # Your values may look something like a DB record:
-            # {
-            #     'id': '123',
-            #     'name':'xxx',
-            #     'title': 'zzz',
-            # }
-
+        # insert an item at given hash key point
         self.buckets[self.hash_key(key)] = val
 
     def get(self, key):
