0037e9a9ce82a35b65cf7094e24fc1462028e598
Peter Kim <seattlechem@yahoo.com>
2018-05-02 21:23:33 -0700

    two error test cases added

--- challenges/print_level_order/print_level_order.py
+++ challenges/print_level_order/print_level_order.py
@@ -3,18 +3,26 @@ from .k_tree import KTree as KT
 from .queue import Queue
 
 
-def print_order_level(input_kt=None):
+def print_level_order(input_kt=None):
     """Print out level-order node values."""
-    if not type(input_kt) is KT:
-        raise TypeError('Input must be K-ary Tree.')
-
     if input_kt is None:
         raise ValueError('Input is missing.')
 
+    if not type(input_kt) is KT:
+        raise TypeError('Input must be K-ary Tree.')
+
     queue = Queue()
-        queue.enqueue(self.root)
-        while queue._length > 0:
-            current = queue.dequeue()
-            print(current.val + )
+    queue.enqueue(input_kt.root)
+    result = ''
+
+    while queue._length > 0:
+        current = queue.dequeue()
+
+        result += '{} '.format(current.val)
+
+        if queue._length == 0:
+            result += '\n'
             for child in current.children:
                 queue.enqueue(child)
+
+    return result
--- challenges/print_level_order/test_print_level_order.py
+++ challenges/print_level_order/test_print_level_order.py
@@ -1,6 +1,23 @@
 """Print level order test."""
+from .print_level_order import print_level_order
 import pytest
 
 
 def test_print_level_order_true(small_tree):
     """Confirm if true for level order print."""
+    result = print_level_order(small_tree)
+    assert result == '5 \n9 3 \n'
+
+
+def test_type_error():
+    """Confirm if error is thrown if input is not K-ary Tree."""
+    with pytest.raises(TypeError) as err:
+        print_level_order(34)
+        assert err is 'Input must be K-ary Tree.'
+
+
+def test_value_error():
+    """Confirm if error is thrown if input is missing."""
+    with pytest.raises(ValueError) as err:
+        print_level_order()
+        assert err is 'Input is missing.'
