51982c49ee35b8981e6f8bbb994a90a42de360ca
Peter Kim <seattlechem@yahoo.com>
2018-05-07 12:00:07 -0700

    mergesort function implemented

--- challenges/mergesort/mergesort.py
+++ challenges/mergesort/mergesort.py
@@ -1,3 +1,18 @@
+"""Merge sort function."""
+import math
+
+
+def mergesort(ls):
+    """Divide the list into two pieces."""
+    if len(ls) == 1:
+        return ls
+
+    center = math.floor(len(ls) / 2)
+    left = ls[0:center]
+    right = ls[center:]
+
+    return merge(mergesort(left), mergesort(right))
+
 
 def merge(left, right):
     """Two lists are merged."""
