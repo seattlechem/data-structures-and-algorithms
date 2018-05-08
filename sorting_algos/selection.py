3a75472a80540725a082306485ae2bac85e99188
Peter Kim <seattlechem@yahoo.com>
2018-05-08 09:32:31 -0700

    error handling added

--- sorting_algos/selection.py
+++ sorting_algos/selection.py
@@ -3,6 +3,17 @@
 
 def selection(ls):
     """Create a function which sort list by selection."""
+
+    if type(ls) is not list:
+        raise TypeError('Input must be a list type.')
+
+    for i in range(0, len(ls)):
+        if not isinstance(ls[i], int):
+            raise TypeError('All elements must be an integer.')
+
+    if len(ls) <= 1:
+        return ls
+
     index_of_min = 0
 
     for i in range(0, len(ls)):
