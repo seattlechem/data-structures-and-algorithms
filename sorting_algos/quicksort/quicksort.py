50d869ca14a8363e5fb695e5e2dc9ffb55ed5d5d
Peter Kim <seattlechem@yahoo.com>
2018-05-08 13:22:32 -0700

    initial skeleton

---
+++ sorting_algos/quicksort/quicksort.py
@@ -0,0 +1,16 @@
+"""Quick sort function."""
+
+
+def quicksort(arr):
+    """."""
+    pass
+
+
+def quicksort_helper(arr, first, last):
+    """."""
+    pass
+
+
+def partition(arr, first, last):
+    """."""
+    pass
