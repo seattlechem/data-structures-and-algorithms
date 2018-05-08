27d8b19d9b55cd239152c46f30734d4905e3672d
Peter Kim <seattlechem@yahoo.com>
2018-05-08 13:52:38 -0700

    initial partition method

--- sorting_algos/quicksort/quicksort.py
+++ sorting_algos/quicksort/quicksort.py
@@ -3,14 +3,24 @@
 
 def quicksort(arr):
     """."""
-    pass
+    quicksort_helper(arr, 0, len(arr) - 1)
 
 
 def quicksort_helper(arr, first, last):
     """."""
-    pass
+    if first < last:
+        split_point = partition(arr, first, last)
+        quicksort_helper(arr, first, split_point - 1)
+        quicksort_helper(arr, split_point + 1, last)
 
 
 def partition(arr, first, last):
     """."""
-    pass
+    pivot_val = arr[first]
+    leftmark = first + 1
+    rightmark = last
+
+    done = False
+
+    while not done:
+        
